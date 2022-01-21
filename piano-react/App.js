f0f86a2af0e93ae2073c2aecfd2a6771052aded4
Andrej.Boyadjiev <andrej.boyadjiev@bit8.com>
2022-01-21 18:00:54 +0200

    clearing

--- piano-react/App.js
+++ piano-react/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import {View} from 'react-native';
 import PianoScreen from './screens/Piano';
 import WelcomeScreen from './screens/Welcome';
 
@@ -36,32 +36,7 @@ const css = {
 		fontSize: "2.8em",
 		textAlign: "center",
 		textTransform: "uppercase"
-	},
-	// light: {
-	// 	color: "#F3F3F3",
-	// 	fontWeight: "300"
-	// },
-	// inputBar: {
-	// 	display: "flex",
-	// 	height: "3.5em"
-	// },
-	// input: {
-	// 	flex: "10",
-	// 	padding: "0",
-	// 	margin: "0",
-	// 	fontSize: "1.8em",
-	// 	border: "none",
-	// 	borderBottom: "3px solid #FFCE00",
-	// 	textIndent: "0.5em"
-	// },
-	// addBtn: {
-	// 	flex: "2",
-	// 	fontSize: "1.15em",
-	// 	backgroundColor: "#FFCE00",
-	// 	border: "none",
-	// 	outline: "none",
-	// 	cursor: "pointer"
-	// }
+	}
 }
 
 export default App;
